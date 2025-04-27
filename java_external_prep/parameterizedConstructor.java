e226d56e86b4527b5831cfe8b5971087a9081bbd
YashwantKora <Yashwantqwerty@gmail.com>
2025-04-27 14:49:17 +0530

    #8

---
+++ java_external_prep/parameterizedConstructor.java
@@ -0,0 +1,19 @@
+//7. Write a Java program to demonstrate default constructor.
+
+public class parameterizedConstructor {
+    public static void main(String[] args) {
+        Box4 b = new Box4(10, 20, 30);
+        b.Volume();
+    }    
+}
+class Box4 {
+    double width, height, depth;
+    Box4(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
+    void Volume() {
+        System.out.println("Volume is: " + width * height * depth);
+    }
+}
