e12ff34f4162b5a2d3886856e1fa6f3dfc4f9e0d
YashwantKora <Yashwantqwerty@gmail.com>
2025-04-27 14:26:20 +0530

    #5

---
+++ java_external_prep/primitiveDatatypes.java
@@ -0,0 +1,21 @@
+//6. Write a Java program to demonstrate primitive datatypes.
+
+public class primitiveDatatypes {
+    public static void main(String[] args) {
+        char a = 'a';
+        int i = 1;
+        float f = 1.1111f;
+        double d = 23.666d;
+        byte b = 4;
+        short s = 23;
+        boolean bool = true;
+
+        System.out.println(a);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(bool);
+    }
+}
