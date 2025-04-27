b64c616b54cf49505d0330273d39d37c1dbb83d0
YashwantKora <Yashwantqwerty@gmail.com>
2025-04-27 16:14:29 +0530

    #12

---
+++ java_external_prep/_while.java
@@ -0,0 +1,25 @@
+//12. Write a Java program to demonstrate while statement.
+
+// public class _while {
+//     public static void main(String[] args) {
+//         int i = 1;
+//         int sum = 0;
+
+//         while(i < 11) {
+//             sum = sum + i;
+//             i++;
+//         }
+//         System.out.println("Sum: " + sum);
+//     }
+// }
+
+public class _while {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 11) {
+
+            System.out.println(i);
+            i++;
+        }
+    }
+}
