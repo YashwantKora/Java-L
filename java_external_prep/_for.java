8ae7b1e8184d61439348254a541f0c3d9f6ba876
YashwantKora <Yashwantqwerty@gmail.com>
2025-04-27 16:09:18 +0530

    #11

---
+++ java_external_prep/_for.java
@@ -0,0 +1,20 @@
+//11. Write a Java program to demonstrate for statement.
+
+// public class _for {
+//     public static void main(String[] args) {
+//         int sum = 0;
+
+//         for (int i = 1; i < 10; i++) {
+//             sum = sum + i;
+//         }
+//         System.out.println("sum: " + sum);
+//     }    
+// }
+
+public class _for {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
+    }
+}
