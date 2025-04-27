fa91d4096664b178e30f74a82a9ebab86eb730e2
YashwantKora <Yashwantqwerty@gmail.com>
2025-04-27 17:47:11 +0530

    #19

---
+++ java_external_prep/auto.java
@@ -0,0 +1,12 @@
+//19. Write a Java program to demonstrate boxing and unboxing.
+
+public class auto {
+    public static void main(String[] args) {
+        int a = 50;
+        Integer a1 = new Integer(a);
+        Integer a2 = 5;
+        System.out.println(a1 + " " + a2);
+        int c = 1;
+        System.out.println(c);
+    }
+}
